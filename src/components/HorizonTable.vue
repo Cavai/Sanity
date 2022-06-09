99aea9a333278ba13a4ea8cf6f308d543ea2842c
Marceli Olszewski <marceliolszewski@gmail.com>
2022-06-09 10:51:05 +0200

    Project update - Horizon commit stats

--- src/components/HorizonTable.vue
+++ src/components/HorizonTable.vue
@@ -14,6 +14,23 @@
       <template slot-scope="{ row }" slot="repository">
         <a :href="row.repository_url" target="_blank">{{ row.repository }}</a>
       </template>
+      <template slot-scope="{ row }" slot="details">
+        <ul class="commit-stats">
+          <Tooltip content="Files changed">
+            <li><Icon type="ios-document-outline" />{{ row.files }}</li>
+          </Tooltip>
+          <Tooltip content="Additions">
+            <li style="color: #3bca51">
+              <Icon type="ios-add" />{{ row.stats.additions }}
+            </li>
+          </Tooltip>
+          <Tooltip content="Deletions">
+            <li style="color: #ff0629">
+              <Icon type="ios-remove" />{{ row.stats.deletions }}
+            </li>
+          </Tooltip>
+        </ul>
+      </template>
     </Table>
   </div>
 </template>
@@ -21,17 +38,21 @@
 <script>
 import moment from "moment";
 
+import octokit from "@/mixins/octokit";
+
 export default {
   name: "HorizonTable",
+  mixins: [octokit],
   props: ["commitsData"],
-  created() {
-    this.tableData = this.commitsData
+  async mounted() {
+    const commits = this.commitsData
       .map((repo) =>
         repo.commits.map((commit) => ({ ...commit, repo: repo.repo }))
       )
       .flat()
       .map((commit) => {
         return {
+          sha: commit.sha,
           date: moment(new Date(commit.commit.author.date)).format("LLLL"),
           commit: commit.commit.message,
           url: commit.html_url,
@@ -39,6 +60,28 @@ export default {
           repository_url: `https://github.com/${process.env.VUE_APP_ORGANISATION}/${commit.repo}`,
         };
       });
+
+    const commitsDetailsPromises = [];
+
+    commits.forEach((commit) => {
+      commitsDetailsPromises.push(
+        this.octokit.repos.getCommit({
+          owner: process.env.VUE_APP_ORGANISATION,
+          repo: commit.repository,
+          ref: commit.sha,
+        })
+      );
+    });
+
+    const commitsDetails = await Promise.all(commitsDetailsPromises);
+
+    this.tableData = commits.map((commit, index) => {
+      return {
+        ...commit,
+        stats: commitsDetails[index].data.stats,
+        files: commitsDetails[index].data.files.length,
+      };
+    });
   },
   data() {
     return {
@@ -69,6 +112,12 @@ export default {
           slot: "repository",
           sortable: true,
           width: 250,
+        },
+        {
+          title: "Details",
+          key: "details",
+          slot: "details",
+          width: 200,
         },
       ],
     };
